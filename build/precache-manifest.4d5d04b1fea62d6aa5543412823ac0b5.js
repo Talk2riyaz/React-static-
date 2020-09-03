07be5116c117088730fa6fdd0ff2dbbc9857ac77
Riyaz Ahemad <65034428+Talk2riyaz@users.noreply.github.com>
2020-09-04 00:20:20 +0700

    Add files via upload

---
+++ build/precache-manifest.4d5d04b1fea62d6aa5543412823ac0b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950cf0b4fafe5b8a0b35f1eddb64ae57",
+    "url": "/Talk2riyaz/React-static-./index.html"
+  },
+  {
+    "revision": "3b24bbfdaa4483c421c6",
+    "url": "/Talk2riyaz/React-static-./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6c7b0ce085dd289585a1",
+    "url": "/Talk2riyaz/React-static-./static/js/2.09095345.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Talk2riyaz/React-static-./static/js/2.09095345.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b24bbfdaa4483c421c6",
+    "url": "/Talk2riyaz/React-static-./static/js/main.99ce98ca.chunk.js"
+  },
+  {
+    "revision": "95b5adad48046826701f",
+    "url": "/Talk2riyaz/React-static-./static/js/runtime-main.597962c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Talk2riyaz/React-static-./static/media/logo.5d5d9eef.svg"
+  }
+]);
